dacd004074edb78b01744866f16563b489802d70
Akash B. Sheth <absheth@iu.edu>
2018-09-07 15:01:36 -0400

    l2 norm implemented

--- assignment1/mat_vec_prod.c
+++ assignment1/mat_vec_prod.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 //TODO
-void load_matrix(long long *matrix_ptr, int *row, int *column) {
+void load_matrix(double **matrix_ptr, int *row, int *column) {
     FILE *fp;
     char *line = NULL;
     size_t len = 0;
@@ -24,31 +25,25 @@ void load_matrix(long long *matrix_ptr, int *row, int *column) {
             *row = atoi(strtok(line, " "));
             *column = atoi(strtok(NULL, " "));
             total_lines = atoi(strtok(NULL, " "));
-            fprintf(stdout, "Rows: %d | ", *row);
-            fprintf(stdout, "Columns: %d | ", *column);
-            fprintf(stdout, "Total lines: %d\n", total_lines);
-            matrix_ptr = (long long *) malloc(sizeof(long long) * (*row) * (*column));
-            memset(matrix_ptr, 0, r*c);
+            //fprintf(stdout, "Rows: %d | ", *row);
+            //fprintf(stdout, "Columns: %d | ", *column);
+            //fprintf(stdout, "Total lines: %d\n", total_lines);
+            *matrix_ptr = (double *) malloc(sizeof(double) * (*row) * (*column));
+            memset(*matrix_ptr, 0, (*row) * (*column) * sizeof(double));
         } else {
             int temp_r, temp_c;
-            long long temp_value;
+            double temp_value;
             char *eptr;
-            // char tmp_str[255];
-            // memset(&tmp_str, 0, 255);
             temp_r = atoi(strtok(line, " "));
             temp_c = atoi(strtok(NULL, " "));
-            // temp_value = strtoll(strtok(NULL, " "), &eptr, 10);
-            // strcpy(tmp_str, strtok(NULL, " "));
-            
-            // temp_value = strtoll(tmp_str, &eptr, 10);
             temp_value = atof(strtok(NULL, " "));
-            fprintf(stdout, "temp_r: %d | ", temp_r);
-            fprintf(stdout, "temp_c: %d | ", temp_c);
-            // fprintf(stdout, "char value: %s | ", tmp_str);
-            fprintf(stdout, "converted : %e \n", temp_value);
-            // fprintf(stdout, "converted : %s\n", eptr);
-            
-             
+            //fprintf(stdout, "temp_r: %d | ", temp_r);
+            //fprintf(stdout, "temp_c: %d | ", temp_c);
+            //fprintf(stdout, "converted : %.13e | ", temp_value);
+            //fprintf(stdout, "line: %d | ", line_count);
+            //fprintf(stdout, "index: %d \n", (temp_r-1) * (*column) + (temp_c-1));
+
+            *(*matrix_ptr + (temp_r-1) * (*column) + (temp_c-1)) = temp_value;
         }
     }
     printf("Final line count: %d\n", line_count);
@@ -61,7 +56,47 @@ void load_matrix(long long *matrix_ptr, int *row, int *column) {
 }
 
 //TODO
-void get_vector() {
+void get_vector(double **vector_ptr, int column) {
+    int i;
+    *vector_ptr= (double *) malloc(sizeof(double) * column);
+
+    memset(*vector_ptr, 0, column * sizeof(double));
+    // for (i = 0; i < column; i++) {
+    //     fprintf(stdout, "i: %d ==> %.13e \n", i, *(vector+ i));
+    // }
+    // sin(2*pi*i/182)
+    for (i = 0; i < column; i++) {
+        // *(*vector + i) = sin((2 * M_PI * i)/182) ;
+        *(*vector_ptr + i) = sin((2*M_PI*i)/182);
+    }
+
+}
+
+mat_vec_product(double **matrix_ptr, double **vector_ptr, double **prod_result_ptr, double *l2_norm, int row, int column) {
+    int i, j, k;
+    double sum = 0.0, prod_sum = 0.0; 
+    
+    *prod_result_ptr = (double *) malloc(sizeof(double) * row);
+    memset(*prod_result_ptr, 0, row * sizeof(double));
+    
+    for (i = 0; i < row; i++) {
+        for(j = 0; j < column; j++) { 
+             prod_sum += *(*matrix_ptr + i * column + j) * *(*vector_ptr + j);
+        }
+        *(*prod_result_ptr + i) = prod_sum;
+        prod_sum = 0; 
+    }
+    for (i = 0; i < row; i++) {
+        sum += pow(*(*prod_result_ptr + i), 2.0);
+         
+    }
+    // for (i = 0; i < row; i++) {
+    //     fprintf(stdout, "i: %d ==> %.13e \n", i, *(*prod_result_ptr+ i));
+    // }
+    *l2_norm = sqrt(sum);
+    
+    
+    fprintf(stdout, "l2_norm : %lf \n", *l2_norm);
 
 }
 
@@ -69,21 +104,38 @@ int main(void) {
     fprintf(stdout, "\n");
     fprintf(stdout, "This is main method\n");
     int i, j;
-    long long *matrix;
+    double *matrix_ptr;
+    double *vector_ptr;
+    double *prod_result_ptr;
+    double l2_norm;
     int row, column;
 
     //TODO
     // Load the matrix
-    load_matrix(matrix, &row, &column);
+    load_matrix(&matrix_ptr, &row, &column);
+
+    // fprintf(stdout, "Rows: %d | ", row);
+    // fprintf(stdout, "Columns: %d \n", column);
     // for (i = 0; i < row; i++) {
-    //     for (j = 0; j < row; j++) {
-    //         fprintf(stdout, "i: %d, j: %d ==> %e", i, j, *(matrix + i*column + j));
+    //     for (j = 0; j < column; j++) {
+    //         fprintf(stdout, "i: %d, j: %d ==> %.13e \n", i, j, *(matrix + i*(column) + j));
     //     }
     //     fprintf(stdout, "\n");
     // }
     //TODO
     // Generate the vector
-    get_vector();
+    get_vector(&vector_ptr, column);
+    // for (i = 0; i < column; i++) {
+    //     fprintf(stdout, "i: %d ==> %.13e \n", i, *(vector_ptr+ i));
+    // }
+
+    mat_vec_product(&matrix_ptr, &vector_ptr, &prod_result_ptr, &l2_norm, row, column);
     fprintf(stdout, "\n");
+
+    // Release memory
+    free(matrix_ptr);
+    free(vector_ptr);
+    free(prod_result_ptr);
+
     return 0;
 }
