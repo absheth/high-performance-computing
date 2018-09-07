31bd63392e832f3febd392be864c14e699c6c4dc
Akash B. Sheth <akashbsheth@gmail.com>
2018-09-07 18:54:45 -0400

    Fixed minor error

--- assignment1/mat_vec_prod.c
+++ assignment1/mat_vec_prod.c
@@ -33,22 +33,22 @@ void load_matrix(double **matrix_ptr, int *row, int *column) {
             temp_r = atoi(strtok(line, " "));
             temp_c = atoi(strtok(NULL, " "));
             temp_value = atof(strtok(NULL, " "));
-            
+
             *(*matrix_ptr + (temp_r-1) * (*column) + (temp_c-1)) = temp_value;
         }
     }
-    
+
     fclose(fp);
     if (line)
         free(line);
 }
 
-// Popluta the vector with formula: sin(2*pi*i/182) 
+// Popluta the vector with formula: sin(2*pi*i/182)
 void get_vector(double **vector_ptr, int column) {
     int i;
     *vector_ptr= (double *) malloc(sizeof(double) * column);
     memset(*vector_ptr, 0, column * sizeof(double));
-    
+
     for (i = 0; i < column; i++) {
         // *(*vector + i) = sin((2 * M_PI * i)/182) ;
         *(*vector_ptr + i) = sin((2*M_PI*i)/182);
@@ -56,54 +56,54 @@ void get_vector(double **vector_ptr, int column) {
 }
 
 void mat_vec_product(double **matrix_ptr, double **vector_ptr, double **prod_result_ptr, double *l2_norm, int row, int column, int times) {
-    
+
     struct timeval start, end;
-    
+
     int i, j, k;
-    double sum = 0.0, prod_sum = 0.0; 
-     
+    double sum = 0.0, prod_sum = 0.0;
+
     *prod_result_ptr = (double *) malloc(sizeof(double) * row);
-    
+
     int x;
     gettimeofday(&start, NULL);
     for (x = 0; x < times; x++) {
-        
+
         memset(*prod_result_ptr, 0, row * sizeof(double));
         for (i = 0; i < row; i++) {
-            for(j = 0; j < column; j++) { 
+            for(j = 0; j < column; j++) {
                 *(*prod_result_ptr + i) += *(*matrix_ptr + i * column + j) * *(*vector_ptr + j);
             }
         }
     }
     gettimeofday(&end, NULL);
-    
-    
+
+
     // fprintf(stdout, "Start | sec: %d, usec %d \n", start.tv_sec, start.tv_usec);
     // fprintf(stdout, "End | sec: %d, usec %d \n", end.tv_sec, end.tv_usec);
-    
+
     double time_taken = ((end.tv_sec + end.tv_usec*1.0e-6) - (start.tv_sec + start.tv_usec*1.0e-6))/(1.0*times);
     for (i = 0; i < row; i++) {
         sum += pow(*(*prod_result_ptr + i), 2.0);
-         
-    } 
+
+    }
     *l2_norm = sqrt(sum);
     fprintf(stdout, "l2_norm : %lf \n", *l2_norm);
     fprintf(stdout, "time_taken: %lf \n", time_taken);
     fprintf(stdout, "FLOPS: %lf \n", (2*row*column)/(time_taken*1000000.0));
-    
+
 }
 
 int main(int argc, char **argv) {
-      
+
     fprintf(stdout, "\n");
     // fprintf(stdout, "Argc --> %d\n", argc);
     int i, j, times;
     if (argc > 1) {
         times = atoi(argv[1]);
     } else {
-        times = 10000;    
+        times = 10000;
         fprintf(stdout, "Running  %d by default and taking average.\n", times);
-        fprintf(stdout, "It can also be run as: ./mat_vev_prod <times>.\n", times);
+        fprintf(stdout, "It can also be run as: ./mat_vev_prod <times>.\n");
         fprintf(stdout, "\n");
     }
     double *matrix_ptr, *vector_ptr, *prod_result_ptr;
@@ -119,7 +119,7 @@ int main(int argc, char **argv) {
     // Peform calculations
     mat_vec_product(&matrix_ptr, &vector_ptr, &prod_result_ptr, &l2_norm, row, column, times);
     fprintf(stdout, "\n");
-    
+
     // fprintf(stdout, "Rows: %d | ", row);
     // fprintf(stdout, "Columns: %d \n", column);
     // for (i = 0; i < row; i++) {
